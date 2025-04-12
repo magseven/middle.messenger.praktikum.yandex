e273867578cbee90d2b1262b4bd1976c3c5d7e1c
eugene <eorlovsky@gmail.com>
2025-04-12 16:47:50 +0300

    defined type Context

--- src/index.ts
+++ src/index.ts
@@ -5,47 +5,47 @@ import header from './templates/header.tmpl'
 import index from './templates/index.tmpl'
 import login from './templates/login.tmpl'
 
+import {getCurrentPage} from './utils/common'
+
 Handlebars.registerPartial( 'button', button);
 Handlebars.registerPartial( 'form_input', form_input);
 Handlebars.registerPartial( 'header', header);
 
 document.addEventListener( 'DOMContentLoaded', () => {
-   const getCurrentPageName = (): string => {
-       const str = window.location.pathname.split('/').filter(part => part !== '');
-       return !str.length ? "index" : str[str.length - 1];
-   };
-
-
-    const context = {
-        templates : [[ 'index', index], [ 'login', login]],
+    const context : {
+        templates: {[key: string]: string},
+        menu: string[][]
+    } = {
+        templates : { index, login},
         menu : [
-                    [ 'login', 'Авторизация'], 
-                    [ 'signin', 'Регистрация'], 
-                    [ 'chats', 'Список чатов'],
-                    [ 'page_404', 'Страница 404'],
-                    [ 'page_500', 'Страница 500'],
-                ]};
+            [ 'login', 'Авторизация'], 
+            [ 'signin', 'Регистрация'], 
+            [ 'chats', 'Список чатов'],
+            [ 'page_404', 'Страница 404'],
+            [ 'page_500', 'Страница 500'],
+        ]
+    };
     
-//    const template = Handlebars.compile( index);
-//    const result = template( context.menu);
-    
-    const currentPage = getCurrentPageName();    
-    const currentTemplate = context.templates.find(template => template[0] === currentPage);
+    const currentPage : string = getCurrentPage();    
+
+    const currentTemplate : string | undefined = context.templates[currentPage] || index;
+
     console.log( 'currentPage', currentPage);                
-    console.log('templ:', currentTemplate[1]);
-    console.log('index:', index);
+    console.log('templ =', currentTemplate[1]);
 
-    const template = Handlebars.compile( currentTemplate);
+    const template = Handlebars.compile( currentTemplate[1]);
 
-    var result;
-    if ( currentTemplate === index)
-        result = template( context.menu);
-    else
-        result = template();
+    const result = () => {
+        if ( currentPage === 'index') {
+            return template(context.menu);
+        }else{
+            return template();
+        }
+    };
 
-    console.log( 'result', result);                
+    console.log( 'result:', result);                
     const root = document.querySelector('#app');
-    root.innerHTML = result;
+    root.innerHTML = result();
     
 })
 
--- src/templates/login.tmpl.ts
+++ src/templates/login.tmpl.ts
@@ -7,23 +7,8 @@ export default `
         {{>form_input name="login" label="Имя пользователя" type="text" required=true}}
         {{>form_input name="password" label="Пароль" type="password" required=true}}
         {{>button type="submit" label="Вход"}}
-        <a class="a-link" href="./signin.html">Еще не зарегистрированы?</a>
+        <a class="a-link" href=index>Еще не зарегистрированы?</a>
       </form>
     </section>
 `;
 
-// {{<page_header title={{title_auth}} }}
-// <body>
-//   <main>
-//     <section class="a-login-container">
-//       {{>header label=title_auth theme_icon="static/cloud.png" theme_icon_descr="������"}}
-//       <form class="a-form">
-//         {{>form_input name="login" label="��� ������������" type="text" required=true}}
-//         {{>form_input name="password" label="������" type="password" required=true}}
-//         {{>button type="submit" label="����"}}
-//         <a class="a-link" href="./signin.html">��� �� ����������������?</a>
-//       </form>
-//     </section>
-//   </main>
-// </body>
-// </html>
