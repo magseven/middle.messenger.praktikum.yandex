a1fdf4069c53552cb47efe2552f7ab84992701c4
Eugene Orlovsky <e.orlovskiy@avoska.ru>
2025-04-17 13:50:09 +0300

    chat-item styles

--- src/modules/chats/chats.tmpl.ts
+++ src/modules/chats/chats.tmpl.ts
@@ -5,11 +5,10 @@ export default `
         <div class="a-chat-frame">
             <div class="a-chat-bar">
                 <div class="a-chat-bar-title">
-                    <a href=login>Профиль</a>
+                    <a class="a-chat-bar-title-link" href=#>Профиль&nbsp;>&nbsp;<a>
                 </div>
                 <div class="a-chat-bar-search">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                    <input type="text" class="a-chat-bar-search-field" placeholder="Поиск" />
+                    <input type="text" class="a-chat-bar-search-field f-inter fa-search" placeholder="&#xf002;&nbspПоиск"/>
                 </div>
                 <div class="a-chat-bar-list">
                     {{#each chats}}{{>chats_bar_item }}{{/each}}
